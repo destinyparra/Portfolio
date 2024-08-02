b3d51fe830b8751911dfce3611e48ee831aa4dc6
Destiny Parra <116921929+DestNp@users.noreply.github.com>
2024-08-01 23:32:08 -0700

    yuhhh

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,6 +5,11 @@
  * add event listener on multiple elements
  */
 
+const addEventListener = function (elements, eventType, callback) {
+    for (let i = 0, len = elements.length; i < len; i++) {
+        elements[i].addEventListener(eventType, callback);
+    }
+}
 
 
 
@@ -19,4 +24,48 @@ const preloader = document.querySelector("[data-preloader]");
 window.addEventListener("DOMContentLoaded", function () {
     preloader.classList.add("loaded");
     document.body.classList.add("loaded");
+});
+
+
+
+
+
+/**
+ * NAVBAR
+ * nabar toggle for mobile
+ */
+
+const navTogglers = document.querySelectorAll("[data-nav-toggler]");
+const navToggleBtn = document.querySelector("[data-nav-toggle-btn]");
+const navBar = document.querySelector("[data-navbar]");
+const overlay = document.querySelector("[data-overlay]");
+
+
+const toggleNavbar = function () {
+    navBar.classList.toggle("active");
+    navToggleBtn.classList.toggle("active");
+    overlay.classList.toggle("active");
+    document.body.classList.toggle("nav-active");
+}
+
+addEventOnElements(navTogglers, "click", toggleNavbar);
+
+
+
+
+
+/**
+ * HEADER
+ * header active when window scroll down to 100px
+ */
+
+const header = document.querySelector("[data-header]");
+
+
+window.addEventListener("scroll", function* () {
+    if (this.window.scrollY >= 100) {
+        header.classList.add("active");
+    } else {
+        header.classList.remove("active");
+    }
 });
